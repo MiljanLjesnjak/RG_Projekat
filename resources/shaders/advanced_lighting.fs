bbb90c7571995de83e074b0804f712cef890d5e0
MiljanLjesnjak <miljanljesnjak@gmail.com>
2024-04-10 00:44:02 +0200

    Dodat blending za advanced lighting

--- resources/shaders/advanced_lighting.fs
+++ resources/shaders/advanced_lighting.fs
@@ -7,21 +7,26 @@ in VS_OUT {
     vec2 TexCoords;
 } fs_in;
 
-uniform sampler2D floorTexture;
+uniform sampler2D diffuse;
 uniform vec3 lightPos;
 uniform vec3 viewPos;
 uniform bool blinn;
 
 void main()
 {
-    vec3 color = texture(floorTexture, fs_in.TexCoords).rgb;
+    vec4 color = texture(diffuse, fs_in.TexCoords).rgba;
+
     // ambient
-    vec3 ambient = 0.05 * color;
+    vec4 ambient = vec4(0.05, 0.05, 0.05, 1.0) * color;
+
+
     // diffuse
     vec3 lightDir = normalize(lightPos - fs_in.FragPos);
     vec3 normal = normalize(fs_in.Normal);
     float diff = max(dot(lightDir, normal), 0.0);
-    vec3 diffuse = diff * color;
+    vec4 diffuse = diff * color;
+
+
     // specular
     vec3 viewDir = normalize(viewPos - fs_in.FragPos);
     vec3 reflectDir = reflect(-lightDir, normal);
@@ -36,6 +41,8 @@ void main()
         vec3 reflectDir = reflect(-lightDir, normal);
         spec = pow(max(dot(viewDir, reflectDir), 0.0), 8.0);
     }
-    vec3 specular = vec3(0.3) * spec; // assuming bright white light color
-    FragColor = vec4(ambient + diffuse + specular, 1.0);
+    vec4 specular = vec4(0.3) * spec; // assuming bright white light color
+
+
+    FragColor = ambient + diffuse + specular;
 }
